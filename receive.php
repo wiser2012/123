49304499187970c7e6cc97dcdf28dc7bae14b222
wiser2012 <ut02162002@yahoo.com.tw>
2018-01-09 11:58:55 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,10 +1,7 @@
 <?php
-
- $json_str = file_get_contents('php://input'); //接收request的body
- $json_obj = json_decode($json_str); //轉JSON格式
-
- $myfile = fopen("log.txt","w+") or die("Unable to open file!");
- fwrite = ($myfile, "\xEF\xBB\xBF".$json_str);//在字串前加入\xEF\xBB\xBF轉成utf8格式
- fclose($myfile);
-
- ?>
+$json_str = file_get_contents('php://input');//接收request的body
+$json_obj = json_decode($json_str);//從string格式轉為jason格式
+$myfile = fopen("log.txt","w+") or die("Unable to open file!");
+fwrite($myfile, "\xEF\xBB\xBF".$json_str);//在字串前加入\xef....轉成utf8格式，以防中文變亂碼
+fclose($myfile);
+?>
