72c57e94a092753b9d66ec05069d24b1742f9511
wiser2012 <ut02162002@yahoo.com.tw>
2018-01-09 14:11:02 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -5,8 +5,9 @@ $json_obj = json_decode($json_str);//從string格式轉為jason格式
  //產生回傳給line server的格式
 	$sender_userid = $json_obj->events[0]->source->userId;
 	$sender_txt = $json_obj->events[0]->message->text;
+	$sender_replyToken = $json_obj->events[0]->replyToken;
   $response = array (
-				"to" => $sender_userid,
+				"reply" => $sender_replyToken,
 				"messages" => array (
 					array (
 						"type" => "text",
@@ -22,7 +23,7 @@ fclose($myfile);
   //回傳給line server
 	$header[] = "Content-Type: application/json";
 	$header[] = "Authorization: Bearer Nzaj6VdKQbbgKu8cYt2jyEZODY35qldsN85WDPV91SQUKCYtqyQzqPh4KIpSQU4sVSKDdtGRquFZZsLZ7U0QT+9N8w5QBIpmSd4sN5h3RecswoLRhtOBb7rSbIyyXSpv9Kb1200atA/cJUSk6OB1oQdB04t89/1O/w1cDnyilFU=";
-	$ch = curl_init("https://api.line.me/v2/bot/message/push");                                                                      
+	$ch = curl_init("https://api.line.me/v2/bot/message/reply");                                                                      
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
 	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
