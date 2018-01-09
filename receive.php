23cc3bf43e2a2598872ba1e51b401ec8af94ef74
wiser2012 <ut02162002@yahoo.com.tw>
2018-01-09 11:53:28 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,9 +1,3 @@
 <?php
- $json_str = file_get_contents('php://input'); //接收request的body
- $json_obj = json_decode($json_str); //轉JSON格式
-
- $myfile = fopen("log.txt","w+") or die("Unable to open file!");
- fwrite = ($myfile, "\xEF\xBB\xBF".$json_str);//在字串前加入\xEF\xBB\xBF轉成utf8格式
- fclose($myfile);
-
- ?>
+  echo "hello i'm wen."
+  ?>
